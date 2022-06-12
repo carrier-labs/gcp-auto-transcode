7b2e82a2b97cdc29b40b012ecf03e86b8a957365
Sam Shelton <sam.shelton@soloworks.co.uk>
2022-06-12 13:38:42 +0100

    feat: add job config

--- processVideo.go
+++ processVideo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	transcoder "cloud.google.com/go/video/transcoder/apiv1"
 	transcoderpb "google.golang.org/genproto/googleapis/cloud/video/transcoder/v1"
@@ -21,10 +22,68 @@ func processVideo(uri string) error {
 	}
 	defer c.Close()
 
+	// Basic Transcoding
 	req := &transcoderpb.CreateJobRequest{
 		Parent: fmt.Sprintf("projects/%s/locations/%s", ProjectId, "europe-west4"),
 		Job: &transcoderpb.Job{
-			InputUri: "",
+			InputUri:  uri,
+			OutputUri: strings.Replace(uri, "/original/", "/transcoded/", 1),
+			JobConfig: &transcoderpb.Job_Config{
+				Config: &transcoderpb.JobConfig{ElementaryStreams: []*transcoderpb.ElementaryStream{
+					{
+						Key: "video_stream0",
+						ElementaryStream: &transcoderpb.ElementaryStream_VideoStream{
+							VideoStream: &transcoderpb.VideoStream{
+								CodecSettings: &transcoderpb.VideoStream_H264{
+									H264: &transcoderpb.VideoStream_H264CodecSettings{
+										BitrateBps:   550000,
+										FrameRate:    60,
+										HeightPixels: 360,
+										WidthPixels:  640,
+									},
+								},
+							},
+						},
+					},
+					{
+						Key: "video_stream1",
+						ElementaryStream: &transcoderpb.ElementaryStream_VideoStream{
+							VideoStream: &transcoderpb.VideoStream{
+								CodecSettings: &transcoderpb.VideoStream_H264{
+									H264: &transcoderpb.VideoStream_H264CodecSettings{
+										BitrateBps:   2500000,
+										FrameRate:    60,
+										HeightPixels: 720,
+										WidthPixels:  1280,
+									},
+								},
+							},
+						},
+					},
+					{
+						Key: "audio_stream0",
+						ElementaryStream: &transcoderpb.ElementaryStream_AudioStream{
+							AudioStream: &transcoderpb.AudioStream{
+								Codec:      "aac",
+								BitrateBps: 64000,
+							},
+						},
+					},
+				},
+					MuxStreams: []*transcoderpb.MuxStream{
+						{
+							Key:               "sd",
+							Container:         "mp4",
+							ElementaryStreams: []string{"video_stream0", "audio_stream0"},
+						},
+						{
+							Key:               "hd",
+							Container:         "mp4",
+							ElementaryStreams: []string{"video_stream1", "audio_stream0"},
+						},
+					},
+				},
+			},
 		},
 	}
 
